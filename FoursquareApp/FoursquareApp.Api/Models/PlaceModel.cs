14ad3f657c9ebe8b9b4342a238d7352ec05d3939
Polina Koleva <polly.akoleva@gmail.com>
2013-08-14 13:14:02 +0300

    Added Models

--- FoursquareApp/FoursquareApp.Api/Models/PlaceModel.cs
+++ FoursquareApp/FoursquareApp.Api/Models/PlaceModel.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using FoursquareApp.Models;
 
 namespace FoursquareApp.Api.Models
 {
     public class PlaceModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public HashSet<string> Images { get; set; }
+
+        public PlaceModel(Place place)
+        {
+            this.Id = place.Id;
+            this.Name = place.Name;
+            this.Latitude = place.Latitude;
+            this.Longitude = place.Longitude;
+        }
+    }
+
+    public class PlaceDetails : PlaceModel
+    {
+        public ICollection<CommentModel> Comments = new List<CommentModel>();
+
+        public ICollection<UserModel> Users = new List<UserModel>();
+
+        public PlaceDetails(Place place)
+            : base(place)
+        {
+            foreach (var comment in place.Comments)
+            {
+                Comments.Add(new CommentModel(comment));
+            }
+
+            foreach (var user in place.Users)
+            {
+                Users.Add(new UserModel(user));
+            }
+        }
     }
 }
--- FoursquareApp/FoursquareApp.Api/Models/UserModel.cs
+++ FoursquareApp/FoursquareApp.Api/Models/UserModel.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using FoursquareApp.Models;
 
 namespace FoursquareApp.Api.Models
 {
     public class UserModel
     {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public UserModel(User user)
+        {
+            this.Id = user.Id;
+            this.Username = user.Username;
+            this.Password = user.Password;
+        }
+    }
+
+    public class UserDetails : UserModel
+    {
+        public ICollection<PlaceModel> Places = new List<PlaceModel>();
+
+        public ICollection<CommentModel> Comments = new List<CommentModel>();
+
+        public UserDetails(User user)
+            : base(user)
+        {
+            foreach (var place in user.Places)
+            {
+                Places.Add(new PlaceModel(place));
+            }
+
+            foreach (var comment in user.Comments)
+            {
+                Comments.Add(new CommentModel(comment));
+            }
+
+        }
     }
 }
