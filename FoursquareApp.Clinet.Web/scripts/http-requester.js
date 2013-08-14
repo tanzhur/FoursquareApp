629e29b5443bd7a8a8b1e1358bee7de4f469fa78
Teodor92 <teodor.ivanov92@gmail.com>
2013-08-15 01:49:29 +0300

    Started work on the Web client - base persister structure ready.

---
+++ FoursquareApp.Clinet.Web/scripts/http-requester.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="libs/jquery-2.0.3.js" />
+/// <reference path="class.js" />
+/// <reference path="libs/jquery-2.0.3.intellisense.js" />
+
+var httpRequester = (function () {
+	function getJson(url, success, error) {
+		$.ajax({
+			url: url,
+			type: "GET",
+			timeout: 5000,
+			contentType: "application/json",
+			success: success,
+			error: error
+		});
+	}
+
+	function postJson(url, data, success, error) {
+		$.ajax({
+			url: url,
+			type: "POST",
+			contentType: "application/json",
+			timeout: 5000,
+			data: JSON.stringify(data),
+			success: success,
+			error: error
+		});
+	}
+
+	function deleteJson(url, success, error) {
+	    $.ajax({
+	        url: url,
+	        type: "DELETE",
+	        timeout: 5000,
+	        contentType: "application/json",
+	        success: success,
+	        error: error
+	    });
+	}
+
+	function putJson(url, data, success, error) {
+	    $.ajax({
+	        url: url,
+	        type: "PUT",
+	        contentType: "application/json",
+	        timeout: 5000,
+	        data: JSON.stringify(data),
+	        success: success,
+	        error: error
+	    });
+	}
+
+	return {
+	    getJson: getJson,
+	    postJson: postJson,
+	    deleteJson: deleteJson,
+        putJson: putJson
+	};
+}());
---
+++ FoursquareApp.Clinet.Web/scripts/persister.js
@@ -0,0 +1,94 @@
+﻿/// <reference path="libs/class.js" />
+/// <reference path="libs/jquery-2.0.3.intellisense.js" />
+/// <reference path="libs/jquery-2.0.3.js" />
+/// <reference path="http-requester.js" />
+
+
+var persisters = (function () {
+	var nickname = localStorage.getItem("nickname");
+	var sessionKey = localStorage.getItem("sessionKey");
+	function saveUserData(userData) {
+		localStorage.setItem("nickname", userData.nickname);
+		localStorage.setItem("sessionKey", userData.sessionKey);
+		nickname = userData.nickname;
+		sessionKey = userData.sessionKey;
+	}
+	function clearUserData() {
+		localStorage.removeItem("nickname");
+		localStorage.removeItem("sessionKey");
+		nickname = "";
+		sessionKey = "";
+	}
+
+	var MainPersister = Class.create({
+		init: function (rootUrl) {
+			this.rootUrl = rootUrl;
+			this.user = new UserPersister(this.rootUrl);
+			this.place = new PlacePersister(this.rootUrl);
+			this.comment = new CommentPersister(this.rootUrl);
+		},
+		isUserLoggedIn: function () {
+			var isLoggedIn = nickname != null && sessionKey != null;
+			return isLoggedIn;
+		},
+		nickname: function () {
+			return nickname;
+		}
+	});
+	var UserPersister = Class.create({
+		init: function (rootUrl) {
+			this.rootUrl = rootUrl + "users/";
+		},
+		login: function (user, success, error) {
+			var url = this.rootUrl + "login";
+			var userData = {
+				username: user.username,
+				authCode: CryptoJS.SHA1(user.username + user.password).toString()
+			};
+
+			httpRequester.postJson(url, userData,
+				function (data) {
+					saveUserData(data);
+					success(data);
+				}, error);
+		},
+		register: function (user, success, error) {
+			var url = this.rootUrl + "register";
+			var userData = {
+				username: user.username,
+				nickname: user.nickname,
+				authCode: CryptoJS.SHA1(user.username + user.password).toString()
+			};
+			httpRequester.postJson(url, userData,
+				function (data) {
+					saveUserData(data);
+					success(data);
+				}, error);
+		},
+		logout: function (success, error) {
+			var url = this.rootUrl + "logout/" + sessionKey;
+			httpRequester.getJson(url, function (data) {
+				clearUserData();
+				success(data);
+			}, error)
+		}
+	});
+
+	var PlacePersister = Class.create({
+	    init: function (rooturl) {
+	        this.rootUrl = rooturl + 'places/';
+	    },
+	});
+
+	var CommentPersister = Class.create({
+	    init: function (rooturl) {
+	        this.rootUrl = rooturl + 'comment/';
+	    },
+	});
+
+	return {
+		get: function (url) {
+			return new MainPersister(url);
+		}
+	};
+}());
