8f49a05e296782d8a6d3f664cf57ddb4bf5f832c
Teodor92 <teodor.ivanov92@gmail.com>
2013-08-14 11:42:53 +0300

    Initial Structure commit

---
+++ FoursquareApp/FoursquareApp.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoursquareApp.Api.WebApiApplication" Language="C#" %>
---
+++ FoursquareApp/FoursquareApp.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoursquareApp.Data
+{
+    public class Class1
+    {
+    }
+}
