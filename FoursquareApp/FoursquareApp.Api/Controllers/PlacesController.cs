45315647e5a8607096201fa3b1628d8ab04250c6
yoandinkov <yoandinkov@gmail.com>
2013-08-14 13:13:12 +0300

    Added constructors to controllers

--- FoursquareApp/FoursquareApp.Api/Controllers/PlacesController.cs
+++ FoursquareApp/FoursquareApp.Api/Controllers/PlacesController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using FoursquareApp.Models;
+using FoursquareApp.Repos;
 
 namespace FoursquareApp.Api.Controllers
 {
     public class PlacesController : ApiController
     {
+        IRepository<Place> placesRepo;
+
+        public PlacesController(IRepository<Place> inputRepo)
+        {
+            this.placesRepo = inputRepo;
+        }
     }
 }
