b4a4d26e96972699f45007695f4172559cf51d7c
Teodor92 <teodor.ivanov92@gmail.com>
2013-08-14 15:05:01 +0300

    Mistery Commit

--- FoursquareApp/FoursquareApp.Api/Controllers/UsersController.cs
+++ FoursquareApp/FoursquareApp.Api/Controllers/UsersController.cs
@@ -6,6 +6,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using FoursquareApp.Models;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace FoursquareApp.Api.Controllers
 {
@@ -30,80 +32,93 @@ namespace FoursquareApp.Api.Controllers
         private const int MinUsernameNicknameChars = 4;
         private const int MaxUsernameNicknameChars = 30;
 
-        private static void ValidateSessionKey(string sessionKey)
+        public static string GetSha1(string value)
         {
-            if (sessionKey.Length != SessionKeyLen || sessionKey.Any(ch => !SessionKeyChars.Contains(ch)))
-            {
-                throw new ServerErrorException("Invalid Password", "ERR_INV_AUTH");
-            }
+            var data = Encoding.ASCII.GetBytes(value);
+            var hashData = new SHA1Managed().ComputeHash(data);
+
+            var hash = string.Empty;
+
+            foreach (var b in hashData)
+                hash += b.ToString("X2");
+
+            return hash;
         }
 
-        private static string GenerateSessionKey(int userId)
-        {
-            StringBuilder keyChars = new StringBuilder(50);
-            keyChars.Append(userId.ToString());
-            while (keyChars.Length < SessionKeyLen)
-            {
-                int randomCharNum;
-                lock (rand)
-                {
-                    randomCharNum = rand.Next(SessionKeyChars.Length);
-                }
-                char randomKeyChar = SessionKeyChars[randomCharNum];
-                keyChars.Append(randomKeyChar);
-            }
-            string sessionKey = keyChars.ToString();
-            return sessionKey;
-        }
+        //private static void ValidateSessionKey(string sessionKey)
+        //{
+        //    if (sessionKey.Length != SessionKeyLen || sessionKey.Any(ch => !SessionKeyChars.Contains(ch)))
+        //    {
+        //        throw new ServerErrorException("Invalid Password", "ERR_INV_AUTH");
+        //    }
+        //}
 
-        private static void ValidateUsername(string username)
-        {
-            if (username == null || username.Length < MinUsernameNicknameChars || username.Length > MaxUsernameNicknameChars)
-            {
-                throw new ServerErrorException("Username should be between 4 and 30 symbols long", "INV_USRNAME_LEN");
-            }
-            else if (username.Any(ch => !ValidUsernameChars.Contains(ch)))
-            {
-                throw new ServerErrorException("Username contains invalid characters", "INV_USRNAME_CHARS");
-            }
-        }
+        //private static string GenerateSessionKey(int userId)
+        //{
+        //    StringBuilder keyChars = new StringBuilder(50);
+        //    keyChars.Append(userId.ToString());
+        //    while (keyChars.Length < SessionKeyLen)
+        //    {
+        //        int randomCharNum;
+        //        lock (rand)
+        //        {
+        //            randomCharNum = rand.Next(SessionKeyChars.Length);
+        //        }
+        //        char randomKeyChar = SessionKeyChars[randomCharNum];
+        //        keyChars.Append(randomKeyChar);
+        //    }
+        //    string sessionKey = keyChars.ToString();
+        //    return sessionKey;
+        //}
 
-        private static void ValidateNickname(string nickname)
-        {
-            if (nickname == null || nickname.Length < MinUsernameNicknameChars || nickname.Length > MaxUsernameNicknameChars)
-            {
-                throw new ServerErrorException("Nickname should be between 4 and 30 symbols long", "INV_NICK_LEN");
-            }
-            else if (nickname.Any(ch => !ValidNicknameChars.Contains(ch)))
-            {
-                throw new ServerErrorException("Nickname contains invalid characters", "INV_NICK_CHARS");
-            }
-        }
+        //private static void ValidateUsername(string username)
+        //{
+        //    if (username == null || username.Length < MinUsernameNicknameChars || username.Length > MaxUsernameNicknameChars)
+        //    {
+        //        throw new ServerErrorException("Username should be between 4 and 30 symbols long", "INV_USRNAME_LEN");
+        //    }
+        //    else if (username.Any(ch => !ValidUsernameChars.Contains(ch)))
+        //    {
+        //        throw new ServerErrorException("Username contains invalid characters", "INV_USRNAME_CHARS");
+        //    }
+        //}
 
-        private static void ValidateAuthCode(string authCode)
-        {
-            if (authCode.Length != Sha1CodeLength)
-            {
-                throw new ServerErrorException("Invalid authentication code length", "INV_USR_AUTH_LEN");
-            }
-        }
+        //private static void ValidateNickname(string nickname)
+        //{
+        //    if (nickname == null || nickname.Length < MinUsernameNicknameChars || nickname.Length > MaxUsernameNicknameChars)
+        //    {
+        //        throw new ServerErrorException("Nickname should be between 4 and 30 symbols long", "INV_NICK_LEN");
+        //    }
+        //    else if (nickname.Any(ch => !ValidNicknameChars.Contains(ch)))
+        //    {
+        //        throw new ServerErrorException("Nickname contains invalid characters", "INV_NICK_CHARS");
+        //    }
+        //}
 
-        /*
-         *  Public request forms 
-         */
-        [HttpPost]
-        [ActionName("register")]
-        public HttpResponseMessage RegisterUser(string username, string password)
-        {
+        //private static void ValidateAuthCode(string authCode)
+        //{
+        //    if (authCode.Length != Sha1CodeLength)
+        //    {
+        //        throw new ServerErrorException("Invalid authentication code length", "INV_USR_AUTH_LEN");
+        //    }
+        //}
+
+        ///*
+        // *  Public request forms 
+        // */
+        //[HttpPost]
+        //[ActionName("register")]
+        //public HttpResponseMessage RegisterUser(string username, string password)
+        //{
             
-        }
+        //}
 
-        [HttpPost]
-        [ActionName("login")]
-        public HttpResponseMessage LoginUser(string username, string password)
-        {
+        //[HttpPost]
+        //[ActionName("login")]
+        //public HttpResponseMessage LoginUser(string username, string password)
+        //{
 
-        }
+        //}
 
     }
 }
---
+++ FoursquareApp/FoursquareApp/FoursquareApp.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FoursquareApp.Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FoursquareApp.Api")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2a97d82-c274-451a-9a4e-e57f192a0c92")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
