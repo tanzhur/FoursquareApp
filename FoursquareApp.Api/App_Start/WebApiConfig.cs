4329bbc91b32a877ad8f022cc2601e7e9e2f5275
Teodor92 <teodor.ivanov92@gmail.com>
2013-08-14 13:41:02 +0300

    Added a GetSha1 Method to the User Controller

--- FoursquareApp.Api/App_Start/WebApiConfig.cs
+++ FoursquareApp.Api/App_Start/WebApiConfig.cs
@@ -15,6 +15,12 @@ namespace FoursquareApp.Api
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            config.Routes.MapHttpRoute(
+                name: "UsersApi",
+                routeTemplate: "{controller}/{action}/{id}",
+                defaults: new {controller = "users", id = RouteParameter.Optional }
+            );
+
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
---
+++ FoursquareApp.Data/FoursquareContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoursquareApp.Models;
+
+namespace FoursquareApp.Data
+{
+    public class FoursquareContext : DbContext
+    {
+        public FoursquareContext()
+            : base("FoursquareDb")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Place> Places { get; set; }
+    }
+}
