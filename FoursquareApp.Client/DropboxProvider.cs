1e4cc2ca7a521228021d31fdc9bd6b589769a3d0
Polina Koleva <polly.akoleva@gmail.com>
2013-08-15 11:37:09 +0300

    Added dropboxProvider

---
+++ FoursquareApp.Client/DropboxProvider.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+using Spring.Social.OAuth1;
+using Spring.Social.Dropbox.Api;
+using Spring.Social.Dropbox.Connect;
+using Spring.IO;
+
+namespace FoursquareApp.Client
+{
+    public class DropboxProvider
+    {
+        public DropboxProvider()
+        { }
+
+        public OAuthToken LoadOAuthToken(string OAuthTokenFileName)
+        {
+            string[] lines = File.ReadAllLines(OAuthTokenFileName);
+            OAuthToken oauthAccessToken = new OAuthToken(lines[0], lines[1]);
+            return oauthAccessToken;
+        }
+
+        public void AuthorizeAppOAuth(DropboxServiceProvider dropboxServiceProvider, string OAuthTokenFileName)
+        {
+            // Authorization without callback url
+            Console.Write("Getting request token...");
+            OAuthToken oauthToken = dropboxServiceProvider.OAuthOperations.FetchRequestToken(null, null);
+            Console.WriteLine("Done.");
+
+            OAuth1Parameters parameters = new OAuth1Parameters();
+            string authenticateUrl = dropboxServiceProvider.OAuthOperations.BuildAuthorizeUrl(
+                oauthToken.Value, parameters);
+            Console.WriteLine("Redirect the user for authorization to {0}", authenticateUrl);
+            Process.Start(authenticateUrl);
+            Console.Write("Press [Enter] when authorization attempt has succeeded.");
+            Console.ReadLine();
+
+            Console.Write("Getting access token...");
+            AuthorizedRequestToken requestToken = new AuthorizedRequestToken(oauthToken, null);
+            OAuthToken oauthAccessToken =
+                dropboxServiceProvider.OAuthOperations.ExchangeForAccessToken(requestToken, null);
+            Console.WriteLine("Done.");
+
+            string[] oauthData = new string[]
+            {
+                oauthAccessToken.Value,
+                oauthAccessToken.Secret
+            };
+            File.WriteAllLines(OAuthTokenFileName, oauthData);
+        }
+
+        public IDropbox Authenticate(DropboxServiceProvider dropboxServiceProvider, DropboxProvider provider, string OAuthTokenFileName)
+        {
+            // Authenticate the application (if not authenticated) and load the OAuth token
+            if (!File.Exists(OAuthTokenFileName))
+            {
+                provider.AuthorizeAppOAuth(dropboxServiceProvider, OAuthTokenFileName);
+            }
+            OAuthToken oauthAccessToken = provider.LoadOAuthToken(OAuthTokenFileName);
+
+            // Login in Dropbox
+            IDropbox dropbox = dropboxServiceProvider.GetApi(oauthAccessToken.Value, oauthAccessToken.Secret);
+            return dropbox;
+        }
+
+        public void TakeUrl(IDropbox dropbox, string image)
+        {
+
+            // Create new folder
+            // string newFolderName = "Test";
+            // Entry createFolderEntry = dropbox.CreateFolder(newFolderName);
+            //Console.WriteLine("Created folder: {0}", createFolderEntry.Path);
+
+            // Upload a file
+            Entry uploadFileEntry = dropbox.UploadFile(new FileResource("../../TestImages/"+image), "/" + image);
+
+            // take url
+            DropboxLink sharedUrl = dropbox.GetShareableLink(uploadFileEntry.Path);
+            Console.WriteLine(sharedUrl.Url);
+        }
+    }
+}
--- FoursquareApp.Client/Program.cs
+++ FoursquareApp.Client/Program.cs
@@ -5,27 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using FoursquareApp.Models;
 using FoursquareApp.Data;
+using Spring.Social.Dropbox.Connect;
+using Spring.Social.Dropbox.Api;
 
 namespace FoursquareApp.Client
 {
     class Program
     {
+        private const string DropboxAppKey = "qo8v1rcsno3proe";
+        private const string DropboxAppSecret = "y1djbdiyn46560r";
+
+        private const string OAuthTokenFileName = "OAuthTokenFileName.txt";
+
         static void Main(string[] args)
         {
             FoursquareContext testContext = new FoursquareContext();
-
+            string image = "img1.jpg";
             //testContext.Users.Add(new User() { Username = "MegaTest" });
             //testContext.SaveChanges();
 
-            foreach (var item in testContext.Users.ToList())
-            {
-                foreach (var p in item.Places)
-                {
-                    Console.WriteLine(p.Name);
-                }
-            }
+            //foreach (var item in testContext.Users.ToList())
+            //{
+            //    foreach (var p in item.Places)
+            //    {
+            //        Console.WriteLine(p.Name);
+            //    }
+            //}
 
-            Console.WriteLine("WTF!");
+            //Console.WriteLine("WTF!");
+            DropboxProvider dropboxProvider = new DropboxProvider();
+
+            DropboxServiceProvider dropboxServiceProvider =
+            new DropboxServiceProvider(DropboxAppKey, DropboxAppSecret, AccessLevel.AppFolder);
+
+            IDropbox dropbox = dropboxProvider.Authenticate(dropboxServiceProvider, dropboxProvider, OAuthTokenFileName);
+
+            dropboxProvider.TakeUrl(dropbox, image);
         }
     }
 }
